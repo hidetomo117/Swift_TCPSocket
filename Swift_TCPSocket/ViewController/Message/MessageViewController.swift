671090a27819cef902f975e737f8bfaecd0d11a3
hidetomo117 <hidetomo117@gmail.com>
2019-06-08 21:30:17 +0900

    change project directory

--- Swift_TCPSocket/ViewController/Message/MessageViewController.swift
+++ Swift_TCPSocket/ViewController/Message/MessageViewController.swift
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MessageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
